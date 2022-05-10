47e9181a34f6fb358be1c7f910b76a22889b8450
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2022-05-09 22:39:39 -0400

    JG-002: Add container - remove it from index.js

--- src/Landing.js
+++ src/Landing.js
@@ -1,4 +1,16 @@
-import { Avatar, Fade, Grid, Hidden, makeStyles, Tooltip, Typography, useMediaQuery, useTheme, Zoom } from "@material-ui/core";
+import {
+  Avatar,
+  Container,
+  Fade,
+  Grid,
+  Hidden,
+  makeStyles,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Zoom
+} from "@material-ui/core";
 import ReactTyped from "react-typed";
 import clsx from "clsx";
 import Image from 'next/image'
@@ -53,51 +65,53 @@ export default function Landing() {
   const mdDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Grid container justify="center" alignItems="center" className={classes.cont}>
-      <Grid item xs={12} lg={6}>
-        <Typography variant={mdDown ? "h2" : "h1"}>
-          {landing.title}
-        </Typography>
-        <Typography variant={mdDown ? "h5" : "h4"} component="h2" className={classes.subtitle}>
+    <Container>
+      <Grid container justify="center" alignItems="center" className={classes.cont}>
+        <Grid item xs={12} lg={6}>
+          <Typography variant={mdDown ? "h2" : "h1"}>
+            {landing.title}
+          </Typography>
+          <Typography variant={mdDown ? "h5" : "h4"} component="h2" className={classes.subtitle}>
 
-          <ReactTyped
-            strings={landing.subtitles}
-            typeSpeed={40}
-            backSpeed={50}
-            loop
-          />
-        </Typography>
-        <Grid container direction="row" spacing={2}>
-          {
-            professionalDetails.map(({ alt, icon, link }, i) =>
-              <Grid item key={i}>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                  <Zoom in={true} style={{ transitionDelay: `${100 * i}ms` }}>
-                    <Tooltip title={alt} placement="top">
-                      <Avatar variant="rounded" className={clsx([classes.avatar, classes[alt]])}>
-                        {icon}
-                      </Avatar>
-                    </Tooltip>
-                  </Zoom>
-                </a>
-              </Grid>
-            )
-          }
-        </Grid>
-      </Grid>
-
-      <Hidden mdDown>
-        <Fade in={true} style={{ transitionDelay: '100ms' }}>
-          <Grid item lg={6}>
-            <Image
-              src="/landing.svg"
-              alt="Landing"
-              width="900.94"
-              height="787"
+            <ReactTyped
+              strings={landing.subtitles}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
             />
+          </Typography>
+          <Grid container direction="row" spacing={2}>
+            {
+              professionalDetails.map(({ alt, icon, link }, i) =>
+                <Grid item key={i}>
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    <Zoom in={true} style={{ transitionDelay: `${100 * i}ms` }}>
+                      <Tooltip title={alt} placement="top">
+                        <Avatar className={clsx([classes.avatar, classes[alt]])}>
+                          {icon}
+                        </Avatar>
+                      </Tooltip>
+                    </Zoom>
+                  </a>
+                </Grid>
+              )
+            }
           </Grid>
-        </Fade>
-      </Hidden>
-    </Grid>
+        </Grid>
+
+        <Hidden mdDown>
+          <Fade in={true} style={{ transitionDelay: '100ms' }}>
+            <Grid item lg={6}>
+              <Image
+                src="/landing.svg"
+                alt="Landing"
+                width="900.94"
+                height="787"
+              />
+            </Grid>
+          </Fade>
+        </Hidden>
+      </Grid>
+    </Container>
   )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,17 @@
-import { Card, CardActionArea, CardActions, CardContent, CardHeader, Chip, Fade, Grid, Hidden, makeStyles, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Chip,
+  Container,
+  Fade,
+  Grid,
+  Hidden,
+  makeStyles,
+  Typography
+} from "@material-ui/core";
 import { RepoForkedIcon, RepoIcon, StarIcon } from '@primer/octicons-react';
 import Image from 'next/image'
 import { useRef } from "react";
@@ -25,85 +38,87 @@ export default function Projects({ projects }) {
   const animate = useAnimate(animRef)
 
   return (
-    <Grid direction="row-reverse" container justify="center" alignItems="center" spacing={10} className={classes.cont}>
-      <Grid item xs={12} lg={6}>
-        <Typography variant="h2" gutterBottom align="center" innerRef={animRef}>
-          Projects
-        </Typography>
-        <Hidden mdDown>
-          <Fade in={animate} style={{ transitionDelay: '250ms' }}>
-            <div>
-              <Image
-                alt="Projects"
-                src="/projects.svg"
-                width="1144"
-                height="617.32"
-              />
-            </div>
-          </Fade>
-        </Hidden>
-      </Grid>
-      <Grid container item xs={12} lg={6} direction="row" spacing={1}>
-        {
-          !!projects && projects.map((project, i) =>
-            <Grid item sm={6} xs={12} key={i}>
-              <Fade in={animate} style={{ transitionDelay: `${200 * i}ms` }}>
-                <Card key={i} className={classes.card}>
-                  <CardActionArea
-                    className={classes.cardActionArea}
-                    href={project.value.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <CardHeader
-                      title={<><RepoIcon verticalAlign='middle' /> {project.value.name}</>}
-                      subheader={
-                        <>
-                          {
-                            !!project.value.stargazers_count &&
-                            <>
-                              <StarIcon verticalAlign='middle' />
-                              {project.value.stargazers_count}
-                            </>
-                          }
-                          {
-                            !!project.value.forks &&
-                            <>
-                              <RepoForkedIcon verticalAlign='middle' />
-                              {project.value.forks}
-                            </>
-                          }
-                        </>
-                      }
-                    />
-                    <CardContent>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                        {project.value.description}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Grid container direction="row" spacing={1}>
-                        {
-                          !!project.value.languages &&
-                          project.value.languages.map((lang, i) =>
-                            <Grid item key={i}>
-                              <Chip
-                                key={i}
-                                label={lang}
-                                size="small"
-                              />
-                            </Grid>
-                          )
+    <Container>
+      <Grid direction="row-reverse" container justify="center" alignItems="center" spacing={10} className={classes.cont}>
+        <Grid item xs={12} lg={6}>
+          <Typography variant="h2" gutterBottom align="center" innerRef={animRef}>
+            Projects
+          </Typography>
+          <Hidden mdDown>
+            <Fade in={animate} style={{ transitionDelay: '250ms' }}>
+              <div>
+                <Image
+                  alt="Projects"
+                  src="/projects.svg"
+                  width="1144"
+                  height="617.32"
+                />
+              </div>
+            </Fade>
+          </Hidden>
+        </Grid>
+        <Grid container item xs={12} lg={6} direction="row" spacing={1}>
+          {
+            !!projects && projects.map((project, i) =>
+              <Grid item sm={6} xs={12} key={i}>
+                <Fade in={animate} style={{ transitionDelay: `${200 * i}ms` }}>
+                  <Card key={i} className={classes.card}>
+                    <CardActionArea
+                      className={classes.cardActionArea}
+                      href={project.value.html_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <CardHeader
+                        title={<><RepoIcon verticalAlign='middle' /> {project.value.name}</>}
+                        subheader={
+                          <>
+                            {
+                              !!project.value.stargazers_count &&
+                              <>
+                                <StarIcon verticalAlign='middle' />
+                                {project.value.stargazers_count}
+                              </>
+                            }
+                            {
+                              !!project.value.forks &&
+                              <>
+                                <RepoForkedIcon verticalAlign='middle' />
+                                {project.value.forks}
+                              </>
+                            }
+                          </>
                         }
-                      </Grid>
-                    </CardActions>
-                  </CardActionArea>
-                </Card>
-              </Fade>
-            </Grid>
-          )
-        }
+                      />
+                      <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                          {project.value.description}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Grid container direction="row" spacing={1}>
+                          {
+                            !!project.value.languages &&
+                            project.value.languages.map((lang, i) =>
+                              <Grid item key={i}>
+                                <Chip
+                                  key={i}
+                                  label={lang}
+                                  size="small"
+                                />
+                              </Grid>
+                            )
+                          }
+                        </Grid>
+                      </CardActions>
+                    </CardActionArea>
+                  </Card>
+                </Fade>
+              </Grid>
+            )
+          }
+        </Grid>
       </Grid>
-    </Grid>
+    </Container>
   )
 }
