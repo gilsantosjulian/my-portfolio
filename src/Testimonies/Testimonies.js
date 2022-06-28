a489ec640b8b6a60349683d68eec2c6606bece2e
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2022-06-27 22:19:13 -0400

    feature: add Testimonies component

--- src/Testimonies/Testimonies.js
+++ src/Testimonies/Testimonies.js
@@ -1,35 +1,16 @@
 import React from 'react'
-import {
-  Container,
-  Grid,
-  makeStyles,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@material-ui/core"
 import clsx from 'clsx'
 import data from '../../data.json'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    borderTop: '1px solid #E6ECF8',
-    maxWidth: '100%',
-    padding: '14.4rem 2.4rem',
-  },
-}))
-
-const Testimonies = ({ companies }) => {
-  const classes = useStyles()
-  const theme = useTheme()
-  const smDown = useMediaQuery(theme.breakpoints.down('md'))
+const Testimonies = ({ }) => {
 
   return (
-    <Container
-      component='section'
-      className={clsx([classes.root, 'root'])}
-    >
-      Testimonies
-    </Container>
+    <section class="section testimonials is-medium has-text-centered has-border-top">
+      <div class="container is-narrow">
+        <h1 class="title is-spaced is-size-3-desktop is-size-4-mobile">Testimonials</h1>
+        <h2 class="subtitle is-size-5-desktop">People I've worked with have said some nice things...</h2>
+      </div>
+    </section>
   )
 }
 
