46aec23b0b9b8372823fd5346e17013622ee0591
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2022-07-27 00:43:15 -0400

    fix: update cta text

--- src/Cta/Cta.js
+++ src/Cta/Cta.js
@@ -10,7 +10,7 @@ const Cta = () => {
               <h1 class="title">Start a project</h1>
             </div>
             <div class="column level-item">
-              <p>Interested in working together? We should queue up a time to chat. I’ll buy the coffee.</p>
+              <p>Interested in working together? Let's talk and drink a coffee.</p>
             </div>
             <div class="column level-item">
               {/* <a class="button is-success is-outlined is-rounded is-medium has-text-weight-normal" href="/project-planner">
