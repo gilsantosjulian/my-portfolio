ea475022d959b3a2020ff2080b52efc61b887df0
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2022-07-27 12:54:12 -0400

    fix: add bulma script to _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Julian Gil Portfolio</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
       </Head>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
